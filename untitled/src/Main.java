b27259f86a5ca217e6b4c75967d24c6c88e893f4
viniciuspandrade12 <vinicius.andrad641@gmail.com>
2025-10-23 20:56:19 -0300

    realizado o desafio da fase 5 em java

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class Main{
     public static void main(String[] args){
-        System.out.println("Olá, Mundo!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("****Crie um programa que solicita ao usuário a insercao de dois numeros e, em seguida, realiza as operacoes aritmeticas basicas (soma, subtracao, multiplicacao e divisao) com esses numeros. Oresultado deve ser exibido na saída padrao****");
+
+
+        System.out.println("Insira um numero: ");
+        double num1 = scanner.nextDouble();
+
+        System.out.println("Insira outro numero: ");
+        double num2 = scanner.nextDouble();
+
+        double soma = num1 + num2;
+        double subtracao = num1 - num2;
+        double multiplicacao = num1 * num2;
+        double divisao = num1 / num2;
+
+        System.out.println(num1 + " + " + num2 + " = " + soma);
+        System.out.println(num1 + " - " + num2 + " = " + subtracao);
+        System.out.println(num1 + " * " + num2 + " = " + multiplicacao);
+        System.out.println(num1 + " / " + num2 + " = " + divisao);
     }
 }
